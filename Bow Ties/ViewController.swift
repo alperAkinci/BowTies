5d4959aa6f4180fb355af12a8ad15a55c303d389
AlperAkinci <alperakinci1@gmail.com>
2016-03-03 16:14:37 +0200

    UI added
    from sample file

---
+++ Bow Ties/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bow Ties
+//
+//  Created by Alper on 03/03/16.
+//  Copyright © 2016 allperr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+}
+
