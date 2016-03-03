49088a244efac0f33752e0deef65fda5d2d77ce5
AlperAkinci <alperakinci1@gmail.com>
2016-03-03 16:31:53 +0200

    Copied SampleData.plist and assets to project

--- Bow Ties/ViewController.swift
+++ Bow Ties/ViewController.swift
@@ -10,10 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var timesWornLabel: UILabel!
+    @IBOutlet weak var lastWornLabel: UILabel!
+    @IBOutlet weak var favoriteLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    @IBAction func segmentedControl(control: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func wear(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func rate(sender: AnyObject) {
+        
+    }
+
+    
+    
 
     
 }
